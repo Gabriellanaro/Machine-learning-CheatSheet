1e517da7a1ded5a4527fd22be264dc367ba972d8
magolimbo <turetta6@gmail.com>
2023-12-11 16:49:05 +0100

    distance between clusters

--- exam_functions.py
+++ exam_functions.py
@@ -48,7 +48,7 @@ def impurity(class_counts, mode='gini'):
 
     return impurity_value
 
-def purity_gain(root, children, purity_measure, accuracy=False):
+def purity_gain(root, children, purity_measure, accuracy=False): #negli esercizi d'esame lo chiama impurity gain ma il nome giusto è purity gain 
     """
     root = list with the size of each class
     children = list of arrays, each array representing the size of each class in a branch after the split
@@ -867,3 +867,34 @@ def naive_bayes(y, df, cols, col_vals, pred_class):
 # pred_class = 1
 # df = pd.DataFrame(a)
 # naive_bayes(y, df, cols, col_vals, pred_class)
+
+
+def distance_between_clusters(self,dist_matrix,cluster1,cluster2):
+        """Returns distance between clusters (avg linkage function)
+
+        Args:
+            dist_matrix (matrix): Distance matrix between observations
+            cluster1: Indexes of cluster 1 observations
+            cluster2: Indexes of cluster 2 observations
+        """
+        distances = []
+        for i in cluster1:
+            for j in cluster2:
+                distances.append(dist_matrix[i][j])
+        distances = np.array(distances)
+        sum = np.sum(distances)
+        elements = len(cluster1) * len(cluster2) 
+        print (sum/elements)
+
+# data = [[0,725,800,150,1000,525,600,500,400,850],
+# [725,0,75,575,275,1250,1325,226,325,125],
+# [800,75,0,650,200,1325,1400,300,400,51],
+# [150,575,650,0,850,675,750,350,250,700],
+# [1000,275,200,850,0,1525,1600,500,600,150],
+# [525,1250,1325,675,1525,0,75,1025,925,1375],
+# [600,1325,1400,750,1600,75,0,1100,1000,1450],
+# [500,226,300,350,500,1025,1100,0,100,350],
+# [400,325,400,250,600,925,1000,100,0,450],
+# [850,125,51,700,150,1375,1450,350,450,0]]
+
+# distance_between_clusters(data, [5,6], [7,8,9])
